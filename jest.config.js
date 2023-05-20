608a64808045dc17e2e500bfee9da0c498536975
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-05-20 13:52:00 +0900

    jest追加

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
