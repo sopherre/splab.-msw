a7cbbb8b617ce9d5b8322f7a65d264f552bfbb0e
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-05-21 20:00:31 +0900

    useNewsの実装

---
+++ src/app/news/hooks/useNews.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from "react";
+import { axios } from "@/lib/axios";
+
+export const useNews = () => {
+  const [success, setIsSuccess] = useState(false);
+  const [news, setNews] = useState({
+    id: "",
+    title: "",
+  });
+
+  const login = useCallback(async () => {
+    try {
+      const response = await axios.post("/login");
+      setIsSuccess(response.data.ok);
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  const fetchNews = useCallback(async () => {
+    try {
+      const res = await fetch("https://localhost:8080/news");
+      const data = await res.json();
+      setNews(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchNews();
+    login();
+  }, [fetchNews, login]);
+
+  return { success, news };
+};
